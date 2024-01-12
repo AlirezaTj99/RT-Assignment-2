8bb58e20f2909fc92ac361329cac1774c7090c94
AlirezaTj99 <tajabadialireza@gmauil.com>
2024-01-12 22:55:30 +0100

    a small update

--- scripts/nodeA.py
+++ scripts/nodeA.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python3
+
+# Import necessary libraries
 import rospy
 from geometry_msgs.msg import Point, Pose, Twist
 from nav_msgs.msg import Odometry
@@ -9,17 +12,20 @@ from assignment_2_2023.msg import PlanningAction, PlanningGoal, PlanningResult
 from std_srvs.srv import SetBool
 from actionlib_msgs.msg import GoalStatus
 
+#defining the class
 class Goal:
     def __init__(self):
+        # Initialize publisher and action client
         self.pub = rospy.Publisher("/pos_vel", Vel, queue_size=1)
         self.client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2023.msg.PlanningAction)
         self.client.wait_for_server()
-        self.goal_cancelled = True
+        self.goal_cancelled = True  # Flag to track if the current goal has been cancelled
 
-    def goalCommand(sself):
+    def goal(self):
         while not rospy.is_shutdown():
-
-            rospy.Subscriber("/odom", Odometry, self.pubPositionVelocity)
+            # Subscribe to /odom topic and publish position and velocity
+            rospy.Subscriber("/odom", Odometry, self.pubPosVelocity)
+            # Get user command
             command = input("If you want to set a goal (press '1') or cancel the current process (press '0')?")
             gPosx = rospy.get_param('/des_pos_x')
             gPosy = rospy.get_param('/des_pos_y')
@@ -31,10 +37,11 @@ class Goal:
 
             if command == '1':
                 try:
+                    # Get new goal coordinates from user
                     inpux = float(input("Where do you wanna go? (in X direction) "))
                     inpuy = float(input("Where do you wanna go? (in Y direction) "))
                 except ValueError:
-                    rospy.logwarn("The input is not acceptable")
+                    rospy.logwarn("The input is not acceptable.")
                     continue
 
                 # Update target position parameters and the goal
@@ -42,11 +49,11 @@ class Goal:
                 rospy.set_param('/des_pos_y', inpuy)
                 goal.target_pose.pose.position.x = inpux
                 goal.target_pose.pose.position.y = inpuy
-
+                
                 # Send the new goal to the action server
                 self.client.send_goal(goal)
                 self.goal_cancelled = False
-                
+
             elif command == '0':
                 if not self.goal_cancelled:
                     # Cancel the current goal if there is one
@@ -60,8 +67,7 @@ class Goal:
 
             rospy.loginfo("Your last input is: [X , Y] = [%f , %f]", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y)
 
-    def pubPositionVelocity(self, msg):
-
+    def pubPosVelocity(self, msg):
         # Extract current position and velocity from the Odometry message
         current_pos = msg.pose.pose.position
         current_vel_linear = msg.twist.twist.linear
@@ -81,7 +87,7 @@ def main():
     # Initialize the node and start handling goal commands
     rospy.init_node('set_target_client')
     handler = Goal()
-    handler.goalCommand()
+    handler.goal()
 
 if __name__ == '__main__':
     main()
